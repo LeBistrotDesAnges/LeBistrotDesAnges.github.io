e7108d4c182afa5e05872440e1de49c8169a6eb2
lebistrotdesanges <audrey.yout@gmail.com>
2016-05-25 09:32:05 +0200

    website content
    website content

---
+++ Js/app.js
@@ -0,0 +1,22 @@
+ (function($){
+
+	 $('.carousel').owlCarousel({
+        items: 3,
+        loop: true,
+        nav:true,
+        navText: ["<img src='img/flat_icones/previous.png'>","<img src='img/flat_icones/next.png'>"],
+        responsive: {
+            0: {
+                items: 1,
+            },
+             481: {
+                items: 2,
+            },
+            961: {
+                items: 3,
+            },
+        }
+    });
+})(jQuery);
+
+
